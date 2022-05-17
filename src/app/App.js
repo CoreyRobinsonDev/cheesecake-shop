8f27e6abf10b44e0cd7262f209d69e8ccaf9bf01
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-05-17 13:57:21 -0400

    added page transitions

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
-
-import { Route, Switch } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
+import { Route, Switch, useLocation } from 'react-router-dom';
 
 import Header from '../components/header/Header';
 import Items  from '../components/items/Items';
@@ -9,23 +9,32 @@ import Success from '../components/success/Success';
 import './App.css';
 
 const App = () => {
-  return <>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={Items} />
-      <Route path='/strawberry' >
-        <OrderForm product='strawberry' />
-      </Route>
-      <Route path='/oreo' >
-        <OrderForm product='oreo' />
-      </Route>
-      <Route path='/sundae' >
-        <OrderForm product='sundae' />
-      </Route>
-      <Route path='/orders' component={Orders} />
-      <Route path='/success' component={Success} />
-    </Switch>
-  </>
+  const location = useLocation();
+  const transtitions = useTransition(location, {
+    from: {transform: 'translate3d(100%,0,0)', opacity: 0 },
+    enter: {transform: 'translate3d(0%,0,0)', opacity: 1 },
+    leave: {transform: 'translate3d(-50%,0,0)', opacity: 0}
+  }) 
+
+  return transtitions((props, item) => (
+    <animated.div style={props}>
+      <Header />
+      <Switch location={item}>
+        <Route exact path="/" component={Items} />
+        <Route path='/strawberry' >
+          <OrderForm product='strawberry' />
+        </Route>
+        <Route path='/oreo' >
+          <OrderForm product='oreo' />
+        </Route>
+        <Route path='/sundae' >
+          <OrderForm product='sundae' />
+        </Route>
+        <Route path='/orders' component={Orders} />
+        <Route path='/success' component={Success} />
+      </Switch>
+    </animated.div>
+  ))
 }
 
 export default App;
